371291edbf3aed7e6b0831c4d26f3faef6f2e3b8
Alejandro Benitez <maxneo4@gmail.com>
2017-09-10 21:13:23 -0500

    filter contacts script

---
+++ PersonalScripts/FilterContactsExported.ps1
@@ -0,0 +1,21 @@
+﻿$content = Get-Clipboard
+$contactsExcel = $content | ConvertFrom-Csv -Delimiter "`t"
+
+$contacts = $contactsExcel | where { $_.mobile1 -ne '' }
+
+
+$contacts = $contacts | select 'Full name', mobile1, mobile2, office1, office2 -Unique
+
+$contactsP = @()
+$lastFullName =''
+foreach($contact in $contacts)
+{
+    if($contact."Full name" -ne $lastFullName)
+    {
+        $contactsP += $contact
+        $lastFullName = $contact."Full name"
+    }
+}
+
+
+$contactsP | Convertto-csv -delimiter "`t" | Set-Clipboard 
